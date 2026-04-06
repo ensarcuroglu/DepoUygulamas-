e31dac335c2b72324f4d8ca314cfd8453ac97221
Ensar Sami Curoglu <ensarcuroglu19@gmail.com>
2026-04-06 16:04:26 +0300

    feat: enable https for local development using mkcert

--- ReactProje/vite.config.js
+++ ReactProje/vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    mkcert(),
   ],
   server: {
+    https: true,
     host: true, // Vite'in 0.0.0.0 üzerinden yerel ağa açılmasını sağlar
     allowedHosts: true, // <-- BÜTÜN TÜNEL LİNKLERİNE İZİN VEREN SATIR
     proxy: {
